3a2cb57a05e3f0a77a8000dc2c91241acaf29db2
Murilo Santos <77702573+muhhx@users.noreply.github.com>
2022-01-26 03:32:27 -0300

    Update firebase.js

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLmQtFWGWnviuAmOkAHmh6V2a8vVsgMwA",
-  authDomain: "task-management-project-58867.firebaseapp.com",
-  projectId: "task-management-project-58867",
-  storageBucket: "task-management-project-58867.appspot.com",
-  messagingSenderId: "571768638320",
-  appId: "1:571768638320:web:942982775bc53ab7259db5"
+  apiKey: _,
+  authDomain: _,
+  projectId: _,
+  storageBucket: _,
+  messagingSenderId: _,
+  appId: _,
 };
 
 const app = initializeApp(firebaseConfig);
